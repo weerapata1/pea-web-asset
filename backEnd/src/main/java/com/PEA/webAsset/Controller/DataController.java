bf07ad8a02c1c23f0b73fbf46b826c1423d8e16e
Weerapat T <45629031+weerapata1@users.noreply.github.com>
2022-03-14 13:56:52 +0700

    update
    Name dataController

--- backEnd/src/main/java/com/PEA/webAsset/Controller/DataController.java
+++ backEnd/src/main/java/com/PEA/webAsset/Controller/DataController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 @CrossOrigin(origins = "*")
-public class DataController {
+public class dataController {
     @Autowired
     private DataRepo DataRepo;
 
