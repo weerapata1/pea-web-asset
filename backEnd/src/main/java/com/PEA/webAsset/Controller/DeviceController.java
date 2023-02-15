e50303d54ca3dc42e7664e6edca1261bedf0817a
looxar <62333313+looxar@users.noreply.github.com>
2023-02-15 13:55:18 +0700

    Feat: check Quota can filter item only 7 year

--- backEnd/src/main/java/com/PEA/webAsset/Controller/DeviceController.java
+++ backEnd/src/main/java/com/PEA/webAsset/Controller/DeviceController.java
@@ -597,4 +597,31 @@ public class DeviceController {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
+
+    @GetMapping("/getDevice53unpageByccIdOnly7Year")
+  public ResponseEntity<Map<String, Object>> getDevice53unpageByccIdOnly7Year(
+    @RequestParam("region") String region,
+    @RequestParam("dt_id") String dt_id
+  ) {
+    try {
+      List<tbDevice> device = new ArrayList<tbDevice>();
+      Pageable paging = Pageable.unpaged();
+
+      Page<tbDevice> pageTuts = deviceRepository.getDevice53unpageByccIdOnly7Year(
+         region, dt_id,  paging
+      );
+      device = pageTuts.getContent();
+
+      Map<String, Object> response = new HashMap<>();
+
+      response.put("totalItems", pageTuts.getTotalElements());
+      response.put("dataDevice", device);
+      // response.put("itemsPerPage", size);
+      return new ResponseEntity<>(response, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
+
+
--- backEnd/src/main/java/com/PEA/webAsset/Repository/DeviceRepository.java
+++ backEnd/src/main/java/com/PEA/webAsset/Repository/DeviceRepository.java
@@ -178,5 +178,13 @@ public interface DeviceRepository extends JpaRepository<tbDevice, Long> {
                         "AND d.dev_pea_no LIKE '53%'", nativeQuery = true)
         Page<tbDevice> getDevice53unpageByccId(@Param("region") String region, @Param("dt_id") String dt_id, Pageable pageable);
 
+        @Query(value = "SELECT * from tb_device d " +
+        // "LEFT JOIN tb_employees e ON d.emp_id = e.emp_id " +
+                        "WHERE d.cc_id LIKE CONCAT(:region,'%') " +
+                        "AND d.dt_id = :dt_id " +
+                        "AND year(d.dev_received_date) >= 2558 "+
+                        "AND d.dev_pea_no LIKE '53%'", nativeQuery = true)
+        Page<tbDevice> getDevice53unpageByccIdOnly7Year(@Param("region") String region, @Param("dt_id") String dt_id, Pageable pageable);
+
         tbDevice findAllByDevPeaNo(String devPeaNo);
 }
